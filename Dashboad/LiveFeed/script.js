0d9a321423c8936243caf3744d3365b63fae116f
Parthiv2749 <87481973+Parthiv2749@users.noreply.github.com>
2024-10-15 19:11:21 +0530

    Website

---
+++ Dashboad/LiveFeed/script.js
@@ -0,0 +1,21 @@
+const socket = new WebSocket('ws://192.168.89.126:10050');  
+const imageElement = document.getElementById('imageDisplay');  
+
+socket.onopen = function(event) {  
+    console.log('Connected to the WebSocket server.');  
+};  
+
+socket.onmessage = function(event) {  
+    print
+    const imgBlob = new Blob([event.data], { type: 'image/jpeg' });  
+    const imgUrl = URL.createObjectURL(imgBlob);  
+    imageElement.src = imgUrl;  
+};  
+
+socket.onerror = function(error) {  
+    console.error('WebSocket Error:', error);  
+};  
+
+socket.onclose = function(event) {  
+    console.log('Disconnected from the WebSocket server.');  
+};  
